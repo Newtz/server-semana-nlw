2a4fe8695a0f3d1c29631feffdc5416b63e74ff6
Newton Peixoto <nowahmst@gmail.com>
2020-06-02 09:44:35 -0300

    config of sqlite database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
-
-app.get('/users', (req, res) => {
-   return res.json('aaa')
-})
+app.use(routes)
 
 app.listen(3333)
