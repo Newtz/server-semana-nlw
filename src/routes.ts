246fb8d10357f81262273a439cc186b93dd46aff
Newton Peixoto <newt.peixoto@gmail.com>
2020-06-05 09:33:25 -0300

    separate config celebrate object

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,8 @@ import multer from 'multer'
 import multerConfig from '../src/config/muter'
 import PointsController from './controllers/PointsController'
 import ItemsController from './controllers/ItemsController'
-import { celebrate, Joi } from 'celebrate'
+import { celebrate } from 'celebrate'
+import { firstArgument,secondArgument} from '../src/Validators/createPointValidator'
 
 const upload = multer(multerConfig)
 const routes = express.Router()
@@ -15,20 +16,7 @@ routes.get('/items', itemsController.index)
 routes.post(
   '/points', 
   upload.single('image'),
-  celebrate({
-      body: Joi.object().keys({
-        name: Joi.string().required(),
-        email: Joi.string().required().email(),
-        whatsapp: Joi.number().required(),
-        latitude: Joi.number().required(),
-        longitude: Joi.number().required(),
-        city: Joi.string().required(),
-        uf: Joi.string().required().max(2),
-        items: Joi.string().required()
-      })
-    }, {
-      abortEarly:false
-    }), 
+  celebrate(firstArgument, secondArgument), 
   pointsController.create)
 
 routes.get('/points/:point_id', pointsController.show)
