ecc41d5999bb22697f6f947d39f38a6006e9c901
Newton Peixoto <newt.peixoto@gmail.com>
2020-06-05 08:22:38 -0300

    config multer and routes to upload file

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,18 @@
 import express from 'express'
+import multer from 'multer'
+import multerConfig from '../src/config/muter'
 import PointsController from './controllers/PointsController'
 import ItemsController from './controllers/ItemsController'
 
+const upload = multer(multerConfig)
 const routes = express.Router()
 const pointsController = new PointsController
 const itemsController = new ItemsController
 
 routes.get('/items', itemsController.index)
-routes.post('/points', pointsController.create)
+
+routes.post('/points', upload.single('image'), pointsController.create)
+
 routes.get('/points/:point_id', pointsController.show)
 routes.get('/points', pointsController.index)
 
