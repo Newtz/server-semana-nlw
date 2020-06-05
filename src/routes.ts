9c27e8ce5a354ca283383cb6e364051c9b6c85b1
Newton Peixoto <newt.peixoto@gmail.com>
2020-06-05 09:30:30 -0300

    using celebrate to validate

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer'
 import multerConfig from '../src/config/muter'
 import PointsController from './controllers/PointsController'
 import ItemsController from './controllers/ItemsController'
+import { celebrate, Joi } from 'celebrate'
 
 const upload = multer(multerConfig)
 const routes = express.Router()
@@ -11,7 +12,24 @@ const itemsController = new ItemsController
 
 routes.get('/items', itemsController.index)
 
-routes.post('/points', upload.single('image'), pointsController.create)
+routes.post(
+  '/points', 
+  upload.single('image'),
+  celebrate({
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required()
+      })
+    }, {
+      abortEarly:false
+    }), 
+  pointsController.create)
 
 routes.get('/points/:point_id', pointsController.show)
 routes.get('/points', pointsController.index)
