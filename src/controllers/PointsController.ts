4abb31dedd37d6c4f2c339d57386a1918466ddcb
Newton Peixoto <newt.peixoto@gmail.com>
2020-06-05 08:38:12 -0300

    return the new url values

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -15,7 +15,7 @@ class PointsController {
       
     const trx = await knex.transaction()
     const insertedId =  await trx('points').insert({ 
-      image:'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60', 
+      image:req.file.filename,
       name, 
       email,
       whatsapp,
@@ -27,7 +27,7 @@ class PointsController {
 
    const  point = {
       id: insertedId[0],
-      image:'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60', 
+      image:`http://192.168.1.101:3333/uploads/${req.file.filename}`,
       name, 
       email,
       whatsapp,
@@ -38,11 +38,14 @@ class PointsController {
     }
     const point_id = insertedId[0];
 
-    const pointItems = items.map( (item_id: number) => {
-      return {
-        item_id,
-        point_id
-      }
+    const pointItems = items
+      .split(',')
+      .map( (item: string ) => Number(item.trim() ) )
+      .map( (item_id: number) => {
+        return {
+          item_id,
+          point_id
+        }
     })
 
     await trx('point_items').insert(pointItems)
@@ -67,6 +70,13 @@ class PointsController {
                   .distinct()
                   .select('points.*')
 
+    const serializedPoints = points.map( point => {
+      return {
+        ...point,
+        image:`http://192.168.1.101:3333/uploads/${req.file.filename}`
+      }
+    })
+
 
     return res.json(points);
   }
@@ -86,7 +96,12 @@ class PointsController {
       })
     }
 
-    return res.json({point, items})
+    const serializedPoint = {
+        ...point,
+        image:`http://192.168.1.101:3333/uploads/${point.image}`
+      }
+
+    return res.json({serializedPoint, items})
   }
 }
 
